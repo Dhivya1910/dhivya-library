70431cebc1c95e97948dd243ce91a47aa8d44922
Dhivya1910 <koteswarandhivya@gmail.com>
2024-08-07 08:35:56 +1000

    added the form component in the app.vue file to create a basic form

--- dhivya-library/src/App.vue
+++ dhivya-library/src/App.vue
@@ -1,23 +1,25 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 import Json from "./components/JSON.vue";
+import Form from "./components/Form.vue"
 import bookstores from "./components/JSON.vue"
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
-  <main>
+  <!-- <main> -->
     <!-- <TheWelcome /> -->
-     <Json/>
-  </main>
+     <!-- <Json/>
+  </main> -->
+  <Form />
 </template>
 
 <style scoped>
